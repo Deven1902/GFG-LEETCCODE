73230e8c82413acfef481e30635ae347b5ba70f4
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-05-29 22:07:45 +0530

    Update details BinarySearch_GFG.cpp

--- array-vectors/BinarySearch_GFG.cpp
+++ array-vectors/BinarySearch_GFG.cpp
@@ -7,6 +7,21 @@ Problem Link:- https://practice.geeksforgeeks.org/problems/binary-search-1587115
 Cpp solution below. 
 */
 
+/* 
+Binary Search Algorithm: The basic steps to perform Binary Search are:
+
+    Begin with the mid element of the whole array as a search key.
+    If the value of the search key is equal to the item then return an index of the search key.
+    Or if the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
+    Otherwise, narrow it to the upper half.
+    Repeatedly check from the second point until the value is found or the interval is empty.
+    
+*/
+
+// time complexitry:- O(log n)
+
+// space:- O(1)
+
 class Solution {
   public:
     int binarysearch(int arr[], int n, int k) {
