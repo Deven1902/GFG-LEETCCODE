ad1ce85ad6ba16c41f41c1eb4f804628e5d48037
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-06-03 12:58:32 +0530

    Update comments

--- array-vectors/BitonicPoint.cpp
+++ array-vectors/BitonicPoint.cpp
@@ -15,20 +15,15 @@ public:
         if ((mid == 0 || arr[mid - 1] <= arr[mid]) &&  (mid == n - 1 || arr[mid + 1] <= arr[mid]))
             return arr[mid];
     
-        // If middle element is not peak and its
-        // left neighbour is greater than it,
-        // then left half must have a peak element
+        // If middle element is not peak and its left neighbour is greater than it, then left half must have a peak element
         
         else if (mid > 0 && arr[mid - 1] > arr[mid])
             return findPeakUtil(arr, low, (mid - 1), n);
     
-        // If middle element is not peak and its
-        // right neighbour is greater than it,
-        // then right half must have a peak element
+        // If middle element is not peak and its right neighbour is greater than it, then right half must have a peak element
         
         else
-            return findPeakUtil(
-                arr, (mid + 1), high, n);
+            return findPeakUtil(arr, (mid + 1), high, n);
     }
 
 	int findMaximum(int arr[], int n) {
