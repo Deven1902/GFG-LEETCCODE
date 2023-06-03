7eddcf3e9fefe7b66a8c5868351f63dcbfdac91f
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-06-03 16:27:01 +0530

    Anagrams naive solution.

---
+++ array-vectors/Anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string c, string d){
+        
+        // Your code here
+        // simple solution -> time complexity:- O(N)
+        //                    space complexity:- no extra space required. 
+                                
+        
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        
+        if(a==b) 
+            return true;
+            
+        else 
+            return false;
+            
+        // optimized solution to be updated. 
+    }
+
+};
