be971fff5dde6729d911325118488619ef064992
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-06-30 06:58:13 +0530

    Update complexity analysis

--- last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,3 +1,18 @@
+// Approach:- 
+/* 
+An easy approach to solve this problem (not Fast)
+
+1. Create a int[][] grid using values of cell.
+
+2. Make a Boolean type DFS function in which we will travel from every cell in first row of grid while making sure all accessible cell of the grid have less value then current day and not revisiting any cell.
+in the main function run binary search between 0 and row*col.
+
+3. Find last maximum day in which we can travel from 0th row to last row using binary search.
+*/
+
+// time complexity:- O(N*row*col);
+// space complexity:- O(row*col);
+
 class Solution {
     public int latestDayToCross(int row, int col, int[][] cells) {
         int grid[][] = new int[row][col];
