a5f61a6040a7db967722436731d0f53ccdc3f587
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-07-09 12:54:59 +0530

    Time: 30 ms (20.62%) | Memory: 18 MB (42.23%) - LeetSync

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // code here
+
+        int res=0;
+
+        for(int i=0;i<nums.size(); i++) 
+            res^=nums[i]^(i+1);
+
+        return res;
+    }
+};
