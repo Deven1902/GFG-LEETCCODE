8133aa0b1dd2d31d3b95b589b37ebdba4fe78f4b
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-07-11 07:49:55 +0530

    Time: 304 ms (28.32%) | Memory: 128 MB (12.35%) - LeetSync

---
+++ spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+ public:
+  vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+    const vector<int> dirs{0, 1, 0, -1, 0};
+    vector<vector<int>> ans(m, vector<int>(n, -1));
+    int x = 0;  // Current x position
+    int y = 0;  // Current y position
+    int d = 0;
+
+    for (ListNode* curr = head; curr; curr = curr->next) {
+      ans[x][y] = curr->val;
+      if (x + dirs[d] < 0 || x + dirs[d] == m || y + dirs[d + 1] < 0 ||
+          y + dirs[d + 1] == n || ans[x + dirs[d]][y + dirs[d + 1]] != -1)
+        d = (d + 1) % 4;
+      x += dirs[d];
+      y += dirs[d + 1];
+    }
+
+    return ans;
+  }
+};
