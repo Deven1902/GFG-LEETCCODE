295bc6dedf2665673aef19dfea54d6fb0f5aec45
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-04 06:57:12 +0530

    Time: 11 ms (49.91%) | Memory: 8.1 MB (69.03%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -7,6 +7,13 @@
  * };
  */
 
+// the below solution is made by two pointer approach. 
+
+// the fast and slow pointer pattern of the questions -> classic example.
+
+// Link to a nice blog to understand the pattern of the problem \U0001f447\U0001f447
+
+// www.educative.io/courses/grokking-coding-interview-patterns-python/fast-and-slow-pointers-introduction
 
 class Solution {
 public:
@@ -23,7 +30,10 @@ public:
             if(slow==fast)  
                 return true;
         }
-        
+
         return false;
     }
 };
+
+// Time complexity:- O(n) 
+// Space Complexity:- O(1) 
