c82e53ca6ca46a4a3694ecd134ab87de5dcf75d9
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-18 09:54:34 +0530

    Time: 104 ms (71.38%) | Memory: 16.7 MB (38.20%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
@@ -2,9 +2,32 @@
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        arr = [(sum(mat[i]), i) for i in range(len(mat))]
+        # arr = [(sum(mat[i]), i) for i in range(len(mat))]
 
-        arr.sort()
+        # arr.sort()
 
-        return [arr[i][1] for i in range(k) ]
+        # return [arr[i][1] for i in range(k) ]
+
+        # Binary Search solution -> alternative apporach
+
+        ans = []
+
+        for i in range(len(mat)):
+            arr = mat[i]
+            low = 0
+            high = len(arr)-1
+            temp_r = 0
+            while low<=high:
+                mid = (low+high)//2
+                if arr[mid]==1:
+                    temp_r = max(temp_r,mid+1)
+                    low = mid+1
+                else:
+                    high = mid-1
+            ans.append((temp_r,i))
+        ans.sort(key = lambda x:x[0])
+        a=[]
+        for i in range(k):
+            a.append(ans[i][1])
+        return a
         
