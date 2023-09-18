95a367025e3a269f4cadcd2e0a255582125a13a6
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-18 09:52:11 +0530

    Time: 96 ms (94.12%) | Memory: 16.6 MB (94.25%) - LeetSync

---
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,10 @@
+#  sorting solution below
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        arr = [(sum(mat[i]), i) for i in range(len(mat))]
+
+        arr.sort()
+
+        return [arr[i][1] for i in range(k) ]
+        
