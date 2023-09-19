b0736a9711b15e7a79a4adcac04350c8eba4f349
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-19 09:04:21 +0530

    Time: 562 ms (33.61%) | Memory: 31.1 MB (55.79%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,3 +1,12 @@
+# task :- to find out the duplicate number between 1 to N
+
+# used sorting apporach, can be done by Two pointers as well
+
+# Apprpoach:- 
+# 1) Sort the given array in ascending order.
+# 2) Iterate through the sorted array.
+# 3) Check if arr[i] is equal to arr[i+1]. If true, arr[i] is the duplicate number.
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
