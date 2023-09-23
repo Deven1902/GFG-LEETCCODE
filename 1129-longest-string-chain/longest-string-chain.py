4addd22f13a1c691889d3ea967e5b588a92ea28c
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-23 18:19:13 +0530

    Time: 110 ms (97.18%) | Memory: 16.7 MB (93.85%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -0,0 +1,18 @@
+class Solution:
+	def longestStrChain(self, words: List[str]) -> int:
+		
+		# code here
+		hash_map = {}
+		words = sorted(words , key = len)
+
+		for word in words:
+			hash_map[word] = 1
+
+			for index in range(len(word)):
+
+				successor = word[:index] + word[index + 1:]
+
+				if successor in hash_map:
+					hash_map[word] = max(hash_map[word] , hash_map[successor] + 1)
+
+		return max(hash_map.values())
