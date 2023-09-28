c27c64febf8971752e735db1b13bb73997aa922a
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-28 09:28:06 +0530

    Update approach to solve the problem!

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,3 +1,16 @@
+# 2 pointer appraoch :- Whenever we encounter odd element at start we swap it with even element at the end side of the array.
+
+# Method
+
+# 1. Even Pointer at start of Array.
+# 2. Odd pointer at end of array.
+# 3. If Even pointer element is even we increase even pointer by 1.
+# 4. If Even pointer element is odd, then we check for odd pointer element.
+# 5. If odd pointer element is odd, we decrease odd pointer by 1.
+# 6. If odd pointer element is even, we SWAP even pointer and odd pointer elements. Increase even pointer by 1 and decrease odd pointer by 1.
+# 7. Use a While loop which will run until even pointer is smaller than odd pointer.
+
+    
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         
