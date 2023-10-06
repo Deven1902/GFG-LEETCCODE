b8ac783c99cb1cd9a58fe4af1e6ab58317a31836
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-06 08:46:03 +0530

    create alternative apporach

---
+++ 343-integer-break/interger-break-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        
+        if n<=3:
+            return n-1
+        k, r=divmod(n,3)
+        if r==1:
+            r=4
+            k-=1
+        ans = 3**k
+        
+        if r>0:
+            ans*=r
+
+        return ans
+
+# time complexity:- O(1)
+
+# space complexity:- O(1)
