ea87c6dfb43d2dfd43449075b6177051b8eb8355
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-11 07:04:00 +0530

    Time: 230 ms (84.75%) | Memory: 82.4 MB (77.31%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -5,20 +5,37 @@ public:
         vector<int> start;
         vector<int> end; 
 
+
+        // interate over vector<flower> and add start and end time of each flower bloom in respective vectors. 
+
         for(int i=0; i<flowers.size(); i++) {
             start.push_back(flowers[i][0]);
             end.push_back(flowers[i][1]);
         }
 
+        // sorting the vectors in asceding order. 
+
         sort(start.begin(), start.end());
         sort(end.begin(), end.end());
 
+        // iterate over the people vector, and and calculate the number of flowers that are in full bloom at the time of each person's visit. 
+
         for (int i = 0; i < people.size(); i++) {
+            
+            // upper_bound() function returns an iterator to the first element in the start
             int blommed_till_now = upper_bound(start.begin(), start.end(), people[i]) - start.begin();
+            // lower_bound() function returns an iterator to the first element in the end
             int bloomed_till_off = lower_bound(end.begin(), end.end(), people[i]) - end.begin();
             ans.push_back(blommed_till_now - bloomed_till_off);
         }
 
+        // number of flowers that are in full bloom at a given time is the difference between the two iterators.
         return ans; 
     }
 };
+
+// Time complexity:- O(n log n) 
+// this is because the function is sorting and iterating over multiple vectors. 
+
+// Space complexity:- O(n)
+// this is because function is storing 2 vectors of n size. 
