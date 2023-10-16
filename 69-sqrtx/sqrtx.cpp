6217b8e36882e8148974e7b1e16f8663a0391609
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-16 08:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (49.73%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,33 @@
+// solved the square root problem using binary search. 
+
+// technique:- reducing the search space to solve in log n time. 
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=0, e=x; //mid = s + (e-s)/2;
+        int ans = -1;
+        long long mid = s + (e-s)/2;
+
+        while(s<=e) {
+            // mid can be ans -> check
+            if(mid*mid == x) return mid; 
+
+            else if(mid*mid < x) {
+                // store ans -> store ans for all the elements who can be less than the actual sqrt/  
+                ans = mid;
+                // move to right
+                s = mid +1;
+            }
+
+            else {
+                // move to left
+                e = mid -1;
+            }
+
+            // update mid before going for next iteration of loop. 
+            mid = s + (e-s)/2;
+        }
+        return ans; 
+    }
+};
