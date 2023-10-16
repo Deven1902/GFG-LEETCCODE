47eab640a9c7bcd6efac570af1478f03b7ec958d
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-16 07:02:29 +0530

    Time: 3 ms (36.93%) | Memory: 6.7 MB (51.35%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    vector<int> getRow(int rowI) {
-        vector<vector<int>> v; // store pascal's traingle
-        vector<int> e; // placeholder to return row
-
-        // iterate through rows from 0 to rowI
-        for(int i=0; i<=rowI; i++) {
-            vector<int> a; //store elements of the current row
-
-            // iterate through elements of the current row
-            for(int j=0; j<=i; j++) {
-                
-                // If it's the first or last element, set it to 1
-                if(j==0 || j==i) {
-                    a.push_back(1);
-                }
-
-                // calculate using elements from the previous row
-                else {
-                    a.push_back(v[i-1][j]+v[i-1][j-1]);
-                }
+    vector<int> getRow(int rowIndex) {
+        vector<int> row = vector<int>(1, 1);
+        for (int i = 1; i <= rowIndex; ++i)
+        {
+            for (int j = row.size() - 1; j > 0; --j)
+            {
+                row[j] = row[j] + row[j-1];
             }
-
-            // return current row. 
-            if(i==rowI) return a;
-            
-            // Add the current row to Pascal's Triangle
-            v.push_back(a);
+            row.push_back(1);
         }
-        
-        // return an empty vector if row not found
-        return e;
+        return row;
     }
 };
 
