1069270f209fdd45264c37324c039465b3188bc1
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-19 08:09:35 +0530

    Added alternative solution

---
+++ 874-backspace-string-compare/backspace.cpp
@@ -0,0 +1,43 @@
+// alternative approach:-  Two pointers.
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int x=0, y=0;
+        
+        // for string s
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '#'){
+                x--;
+                x = max(0, x);
+            }
+            else{
+                s[x] = s[i];
+                x++;
+            }
+        }
+        // for string t
+        for(int i=0; i<t.size(); i++){
+            if(t[i] == '#'){
+                y--;
+                y = max(0, y);
+            }
+            else{
+                t[y] = t[i];
+                y++;
+            }
+        }
+        
+        if(x != y) return false;
+        else{
+            for(int i=0; i<x; i++){
+                if(s[i] != t[i]) return false;
+            }
+            return true;
+        }
+    }
+};
+
+// same time complexity. 
+
+// but the space complexity is O(1). 
