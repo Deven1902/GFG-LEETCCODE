8a5a54b51a6d3b219c458f3423f73be6141eeaad
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-11-30 07:21:51 +0530

    Time: 23 ms (100.00%) | Memory: 16.4 MB (27.27%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        
+        binary = format(n, "b")
+        N, res = len(binary), 0
+        
+        for i in range(1, N+1):
+            if binary[-i] == "1": res = 2**i-1 - res
+                
+        return res
+
+# time complexity:- O(N)
+# space complexity:- O(1)
