9804094ff31e12a2115ec37d0b0cb3abb188a9fd
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-05 07:55:12 +0530

    Time: 31 ms (90.26%) | Memory: 16.2 MB (71.13%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -1,7 +1,7 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         # code here
-        
+
         ans = 0
         while n > 1:
             if n%2 != 0: # if no of teams is odd
@@ -11,3 +11,6 @@ class Solution:
                 ans += int(n/2)
                 n = int(n/2)
         return ans
+
+# time complexity: o(log N)
+# space complexity: o(log N)
