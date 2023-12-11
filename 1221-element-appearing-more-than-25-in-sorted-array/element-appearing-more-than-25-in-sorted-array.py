75557514e7694f4137eef860c483b82a02f7d249
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-11 07:45:29 +0530

    Time: 90 ms (62.69%) | Memory: 17.7 MB (48.07%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.py
@@ -1,8 +1,18 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        n = len(arr) 
+        
+        # find length of arr and store in a variable
+        n = len(arr)  
+
+        # quarter of length of arr is calculated and kept as an integer
         quarter = n // 4
 
+        # loop iterates through the array indices from 0 to (n - quarter - 1) 
+        # this checks that there are elements more than a quatrter to iterate for, if not return. 
         for i in range(n - quarter):
+
+            # checks if the element is same as element 2 quarter steps ahead
             if arr[i] == arr[i + quarter]:
+                
+                # return ans
                 return arr[i]
