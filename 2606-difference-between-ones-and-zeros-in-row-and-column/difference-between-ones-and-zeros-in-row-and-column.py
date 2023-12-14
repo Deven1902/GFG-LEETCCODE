dfc99d658023dad96b0b54463c03f30f11f56709
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-14 08:30:52 +0530

    Time: 1338 ms (88.93%) | Memory: 50.9 MB (73.93%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,12 +1,14 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        m=len(grid)
-        n=len(grid[0])
-        diff=[[0]*n for _ in range(m)]
-        r=[sum(i) for i in grid]
-        c=[sum(k[i] for k in grid) for i in range(n)]
-        x=m+n
+        m = len(grid)  # Number of rows
+        n = len(grid[0])  # Number of columns
+        diff = [[0] * n for _ in range(m)]  # Initialize difference matrix with zeros
+        r = [sum(i) for i in grid]  # List containing number of ones in each row
+        c = [sum(k[i] for k in grid) for i in range(n)]  # List containing number of ones in each column
+        x = m + n  # Total number of cells in the grid
+
         for i in range(m):
             for j in range(n):
-                diff[i][j]=2*(r[i]+c[j])-x
+                diff[i][j] = 2 * (r[i] + c[j]) - x  # Calculate difference for each cell
+
         return diff
