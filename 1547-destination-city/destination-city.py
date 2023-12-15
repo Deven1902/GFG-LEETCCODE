2027ceffecf17676edd043c3f2cfee6ec72c5e67
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-15 06:50:48 +0530

    Time: 50 ms (97.61%) | Memory: 16.2 MB (95.14%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,14 +1,20 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
+        # collecting initial cities in set
         initCity = set()
 
         for p in paths:
             initCity.add(p[0])
 
+        # collecting departure cities . 
         for p in paths:
             destCity = p[1]
 
+            # return a city if not present in both. 
             if destCity not in initCity:
                 return destCity
 
         return ""
+
+        # time complexity:- o(N) -> for loop iteration
+        # space complexity:- O(N) -> set storage sapace
