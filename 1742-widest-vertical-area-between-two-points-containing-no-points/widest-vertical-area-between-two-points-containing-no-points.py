e6655b6ccd26c8665ee9250c7c5cbac0650fca33
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-21 06:34:09 +0530

    Time: 659 ms (100.00%) | Memory: 59.9 MB (6.90%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-        
-        arr=sorted(x for x,y in points);max_dist=0
-        for i in range(1,len(arr)):
-            #finding distance between two points
-            diff=arr[i]-arr[i-1]
-            #if difference is max than current max_dist update it
-            if max_dist<diff: max_dist=diff
+        # extracting x-cordinates from the points and sortng them 
+        arr=sorted(x for x,y in points)
+        # initializing maximum distance:
+        maxDist=0
 
-        return max_dist
+        # iterates through the sorted x-coordinates, starting from the second element (index 1) up to the last element.
+        for i in range(1,len(arr)):
+
+            # finding distance between two points
+            # represents the width of the potential vertical area 
+            diff=arr[i]-arr[i-1]
+
+            # if difference is max than current max_dist update it
+            
+            if maxDist < diff: 
+                maxDist = diff
+
+        # returns the largest distance found
+        return maxDist
+
+# time complexity of O(n log n) 
+# space complexity of O(n)
