41ff053e22e6eb6a0b4138ca990978670b4b7177
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-12-27 09:14:16 +0530

    Time: 3 ms (72.06%) | Memory: 7.2 MB (27.00%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         while(s.find(part) != string::npos) {
+
+            // if in loop, substring still exsists, keep iterating
             s.erase(s.find(part), part.length());
         }
         return s;
