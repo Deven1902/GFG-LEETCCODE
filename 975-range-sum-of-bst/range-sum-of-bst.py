a22e2ffe670a48dc65d2ce2ac34f128d1fbee99b
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2024-01-08 07:24:12 +0530

    Time: 134 ms (84.53%) | Memory: 24.4 MB (87.42%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        # using preorder traversal 
+
+        # just traverse the tree and then increment sum using recurssion.  
+
+        ans = 0
+
+        def preOrder(r, l, h):
+            nonlocal ans
+            if not r: 
+                return
+            
+            if l<=r.val<=h:
+                ans +=r.val
+
+            preOrder(r.left, l, h)
+            preOrder(r.right, l, h) 
+
+        preOrder(root, low, high)
+
+        return ans
