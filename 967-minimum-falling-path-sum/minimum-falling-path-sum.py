4c7bb705d4d80f6c0d838ae9a961c2842781d3b7
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2024-01-19 08:28:28 +0530

    Time: 143 ms (41.64%) | Memory: 18.4 MB (30.83%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -3,4 +3,7 @@ class Solution:
         for row in range(1,len(matrix)):
             for col in range(len(matrix)):
                 matrix[row][col] = matrix[row][col]+min([matrix[row-1][c+col] if (0<=c+col<len(matrix)) else 10001 for c in range(-1,2)])
-        return min(matrix[-1])
+        return min(matrix[-1]) 
+
+        # time complexiyt:- o(N^2) -> two nested loops 
+        # O(1):- instead of temp row, we used matrix and saved the space. 
